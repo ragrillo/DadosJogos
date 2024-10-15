30ccb9601efe8e775f9487ec87ef278092d9172f
Rafael Grillo <79549724+ragrillo@users.noreply.github.com>
2024-10-14 22:53:48 -0300

    atualizando comandos Get

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 })
 
 
-app.get("/", async (req, res) =>{
+app.get("/6ano", async (req, res) =>{
 
     const auth =  new google.auth.GoogleAuth({
         keyFile: "credentials.json",
@@ -51,7 +51,7 @@ app.get("/", async (req, res) =>{
         auth,
         spreadsheetId,
 
-        range: "Jogos!A1:E17",
+        range: "6anoTarde!A4:E27",
 
     });
 
@@ -73,6 +73,86 @@ app.get("/", async (req, res) =>{
 
     res.send(getRows.data)
 });
+
+
+app.get("/7ano", async (req, res) =>{
+
+    const auth =  new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes:"https://www.googleapis.com/auth/spreadsheets",
+
+    });
+
+    //Criando instancia do cliente
+    const client = await auth.getClient();
+
+
+
+    //Criando a instancia do Google Sheets API
+    const googleSheets = google.sheets({version: "v4", auth:client});
+
+    const spreadsheetId = "1-eTApSiLdEpTxGOZWKM1NPKKPNBw9PfZSVw6CyOqI1Y"
+
+    //Get metadata abou spreadsheet
+
+    const metadata = await googleSheets.spreadsheets.get({
+        auth,
+        spreadsheetId,
+    });
+
+    const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+
+        range: "7anoTarde!A4:E27",
+
+    });
+
+
+ 
+
+    res.send(getRows.data)
+});
+
+app.get("/8e9ano", async (req, res) =>{
+
+    const auth =  new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes:"https://www.googleapis.com/auth/spreadsheets",
+
+    });
+
+    //Criando instancia do cliente
+    const client = await auth.getClient();
+
+
+
+    //Criando a instancia do Google Sheets API
+    const googleSheets = google.sheets({version: "v4", auth:client});
+
+    const spreadsheetId = "1-eTApSiLdEpTxGOZWKM1NPKKPNBw9PfZSVw6CyOqI1Y"
+
+    //Get metadata abou spreadsheet
+
+    const metadata = await googleSheets.spreadsheets.get({
+        auth,
+        spreadsheetId,
+    });
+
+    const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+
+        range: "8anoTarde!A4:E36",
+
+    });
+
+
+ 
+
+    res.send(getRows.data)
+});
+
 
 
 
