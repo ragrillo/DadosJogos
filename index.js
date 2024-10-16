bf8363a6173a824979d42789d779f087706195a9
Rafael Grillo <79549724+ragrillo@users.noreply.github.com>
2024-10-15 23:55:51 -0300

    adicionando tabelas em

--- index.js
+++ index.js
@@ -153,6 +153,44 @@ app.get("/8e9ano", async (req, res) =>{
     res.send(getRows.data)
 });
 
+app.get("/medioano", async (req, res) =>{
+
+    const auth =  new google.auth.GoogleAuth({
+        keyFile: "credentials.json",
+        scopes:"https://www.googleapis.com/auth/spreadsheets",
+
+    });
+
+    //Criando instancia do cliente
+    const client = await auth.getClient();
+
+
+
+    //Criando a instancia do Google Sheets API
+    const googleSheets = google.sheets({version: "v4", auth:client});
+
+    const spreadsheetId = "1-eTApSiLdEpTxGOZWKM1NPKKPNBw9PfZSVw6CyOqI1Y"
+
+    //Get metadata abou spreadsheet
+
+    const metadata = await googleSheets.spreadsheets.get({
+        auth,
+        spreadsheetId,
+    });
+
+    const getRows = await googleSheets.spreadsheets.values.get({
+        auth,
+        spreadsheetId,
+
+        range: "Medio!A4:60",
+
+    });
+
+
+ 
+
+    res.send(getRows.data)
+});
 
 
 
